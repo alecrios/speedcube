50fc4eecc2f663dd4f742d881ed7c979dbe63f6d
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-02-09 00:23:47 -0600

    Developed stats component

--- src/components/TheStats.vue
+++ src/components/TheStats.vue
@@ -1,50 +1,124 @@
 <template>
-	<div class="stats">
-		<div class="stat">
-			<div class="label">Best</div>
-			<div class="value">{{ best | formatTime }}</div>
-		</div>
+	<div>
+		<div
+			class="set"
+			v-for="(set, index) in sets"
+			:key="index"
+		>
+			<div class="heading">{{ set.title }}</div>
 
-		<div class="stat">
-			<div class="label">Worst</div>
-			<div class="value">{{ worst | formatTime }}</div>
-		</div>
+			<div class="stats">
+				<div class="stat best">
+					<div class="label">Best</div>
+					<div class="value">{{ getBestTime(set.limit) }}</div>
+				</div>
 
-		<div class="stat">
-			<div class="label">Mean</div>
-			<div class="value">{{ mean | formatTime }}</div>
+				<div class="stat worst">
+					<div class="label">Worst</div>
+					<div class="value">{{ getWorstTime(set.limit) }}</div>
+				</div>
+
+				<div class="stat average">
+					<div class="label">Average</div>
+					<div class="value">{{ getAverageTime(set.limit) }}</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+import solveIdsOfCurrentSession from '@/mixins/solveIdsOfCurrentSession';
+
 export default {
 	name: 'TheStats',
+	mixins: [solveIdsOfCurrentSession],
+	data() {
+		return {
+			sets: [
+				{
+					title: 'Session',
+					limit: null,
+				},
+				{
+					title: 'Latest 5',
+					limit: 5,
+				},
+				{
+					title: 'Latest 12',
+					limit: 12,
+				},
+			],
+		};
+	},
 	computed: {
 		times() {
-			return this.$store.state.solveIds
+			return this.$_solveIdsOfCurrentSession
 				.map((solveId) => this.$store.state.solves[solveId])
-				.filter((solve) => !solve.dnf)
-				.map((solve) => solve.time);
+				.map((solve) => (solve.dnf ? Infinity : solve.time));
 		},
-		best() {
-			return this.times.length ? Math.min(...this.times) : null;
+	},
+	methods: {
+		getLastNTimes(n) {
+			return n === null ? this.times : this.times.slice(0, n);
 		},
-		worst() {
-			return this.times.length ? Math.max(...this.times) : null;
+		getBestTime(n) {
+			const times = this.getLastNTimes(n);
+
+			const best = times.length < n ? null : Math.min(...times);
+
+			return this.getFormattedTime(best);
 		},
-		mean() {
-			return this.times.length
-				? Math.floor(this.times.reduce((sum, time) => sum + time) / this.times.length)
-				: null;
+		getWorstTime(n) {
+			const times = this.getLastNTimes(n);
+
+			const worst = times.length < n ? null : Math.max(...times);
+
+			return this.getFormattedTime(worst);
+		},
+		getAverageTime(n) {
+			const times = this.getLastNTimes(n);
+			let average;
+
+			if (times.length < n) {
+				average = null;
+			} else if (times.includes(Infinity)) {
+				average = Infinity;
+			} else {
+				average = times.reduce((sum, time) => sum + time, 0) / times.length;
+			}
+
+			return this.getFormattedTime(average);
+		},
+		getFormattedTime(time) {
+			if (time === null) return '...';
+
+			return time === Infinity ? 'DNF' : this.$options.filters.formatTime(time);
 		},
 	},
 };
 </script>
 
 <style scoped>
+.set {
+	padding: 1.5rem;
+}
+
+.set + .set {
+	border-top: .125rem solid var(--color-background-lighter);
+}
+
+.heading {
+	font-size: 1.5rem;
+	line-height: 1.5rem;
+	color: var(--color-white);
+	text-align: center;
+	padding: 1.5rem 0;
+}
+
 .stats {
 	display: flex;
+	flex-wrap: wrap;
 	padding: 1.5rem;
 }
 
@@ -54,16 +128,22 @@ export default {
 	text-align: center;
 }
 
+.stat.best .value    {color: var(--color-cube-green)}
+.stat.worst .value   {color: var(--color-cube-red)}
+.stat.average .value {color: var(--color-cube-blue)}
+
 .label {
 	font-size: .75rem;
 	line-height: 1.5rem;
 	font-weight: 500;
 	text-transform: uppercase;
 	letter-spacing: .0625rem;
+	color: var(--color-white);
 }
 
 .value {
-	font-size: 1.25rem;
-	line-height: 1.5rem;
+	font-size: 1.5rem;
+	line-height: 2.25rem;
+	color: var(--color-white);
 }
 </style>
