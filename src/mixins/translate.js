4b30a4b7eba5669af2a9e774bbde2f604ac596e0
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-03-07 22:33:59 -0600

    Added default locale

--- src/mixins/translate.js
+++ src/mixins/translate.js
@@ -1,9 +1,12 @@
 import translations from '@/translations';
 
+const defaultLocale = 'en';
+
 export default {
 	methods: {
 		$t(message) {
-			return translations[this.$store.state.settings.locale][message];
+			return translations[this.$store.state.settings.locale][message]
+				|| translations[defaultLocale][message];
 		},
 	},
 };
