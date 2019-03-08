ad2194a8f63af864d4b0fbacee7ed7ae011d0bad
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-03-08 04:12:34 -0600

    Reworked icons and manifest

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	pwa: {
+		appleMobileWebAppCapable: 'yes',
+		appleMobileWebAppStatusBarStyle: 'black-translucent',
+		themeColor: '#17191c',
+		msTileColor: '#17191c',
+	},
+};
